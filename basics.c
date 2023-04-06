96484fc6d6a0c8c316648628884e7782ba912c24
Aditi-exe <65175049+Aditi-exe@users.noreply.github.com>
2023-04-06 22:30:26 +0530

    Update basics.c

--- basics.c
+++ basics.c
@@ -4,20 +4,22 @@
 
 int main()
 {
-  int x;
-  float f;
-  char ch;
+    int x;
+    float f;
+    char ch;
   
-  printf("Enter an integer: ");
-  scanf("%d", &x);
+    printf("Enter an integer: ");
+    scanf("%d", &x);
   
-  printf("Enter a floating point number: ");
-  scanf("%f", &f);
+    printf("Enter a floating point number: ");
+    scanf("%f", &f);
   
-  printf("Enter a character: ");
-  scanf("%c", &ch);
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+
+    printf("\n");
   
-  printf("Integer: %d\nFloat: %f\nCharacter: %c\n", x, f, ch);
+    printf("Integer: %d\nFloat: %f\nCharacter: %c\n", x, f, ch);
   
-  return 0;
+    return 0;
 }
